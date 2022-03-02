75e168c7b4d8f8045664450a5fa8a43d130c1702
Sajal <chaudharysajal93@gmail.com>
2022-03-02 16:19:32 +0530

    dummy code

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import RichText from './RichText'
+//This is for dummy purpose
 function App() {
 
 
   return (
     <div className="App">
      <RichText/>
+     
     </div>
   );
 }
