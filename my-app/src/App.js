90027642a3d08c95c41ac33d190e242d1dba74ee
Sajal <chaudharysajal93@gmail.com>
2022-03-02 16:24:13 +0530

    Changed again

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
 import RichText from './RichText'
-//This is for dummy purpose
 function App() {
 
 
   return (
     <div className="App">
      <RichText/>
-     
     </div>
   );
 }
